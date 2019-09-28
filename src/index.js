83b03507133944c87d6faac905e08bc220d70d6d
Allan Wilson <allan@allanwilson.net>
2019-09-28 20:17:12 +0300

    .

--- src/index.js
+++ src/index.js
@@ -1,15 +1,47 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const App = () => {
+const RecipeDropdownList = (props) => {
+  console.log(props.recipes[1].name)
   return (
     <div>
       <select>
-        <option value="0">Select recipe:</option>
-        <option value="1">Bataati Keitto</option>
-        <option value="2">Nakki Keitto</option>
-        <option value="3">Carbonara</option>
+          <option value="0">Select recipe:</option>
+          <option value="1">{props.recipes[0].name}</option>
+          <option value="2">{props.recipes[1].name}</option>
+          <option value="3">{props.recipes[2].name}</option>
       </select>
+    </div>
+  )
+}
+
+
+const App = () => {
+  const recipes = [
+      {
+        name: 'Bataatti Porkkana Kookoskeitto',
+        recipe_text: 'Iso bataatti. 2-3 porkkanaa'
+      },
+      {
+        name: 'Pytti Pannu',
+        recipe_text: 'Fry one garlic clove, Boil 6 tatties (skin on), peel and cube'
+      },
+      {
+        name: 'Carbonara',
+        recipe_text: 'Panchetta (Italian fine bacon) - or other salty strips of bacon. Maybe 200g?'
+      }
+  ]
+
+  return (
+    <div>
+      <div>
+        <h1>Cookbook</h1>
+        <RecipeDropdownList recipes={recipes} />
+        <button type="button">Add Recipe</button>
+      </div>
+      <div>
+        <h1>Sports and Hobbies</h1>
+      </div>
     </div>
   )
 }
