51746aefe5dccb8ac98de06c54fb35e06c7c73d6
Allan Wilson <allan@allanwilson.net>
2019-10-04 00:04:12 +0300

    recipes_object as imported object

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
+import {recipes} from './recipes_object'
 
 
 const handleRecipeButtonClick = (props) => {
@@ -21,6 +22,7 @@ const RecipeDropdownList = (props) => {
 }
 
 const renderRecipe = (props) => {
+       console.log("props value", props)
        const x = document.getElementById('recipeSelect');
        const recipeIndex = x.options[x.selectedIndex].value;
        if (recipeIndex === "x") {
@@ -43,31 +45,6 @@ const App = () => {
     renderRecipe({recipes, recipeId})
     setRecipeId(props.recipeId)
   }
-
-
-
-  const recipes = [
-      {
-        name: 'Asian Noodle Salad',
-        ingredients: 'Chopped: Spring Onion<br>Chopped: Chinese Cabbage or Lettuce<br>Crushed: Cashew nuts or almonds',
-        recipe_text: 'Mix while heating gently and pour over:<br>Olive Oil     1/2 cup<br>Caster Suger  1/4 cup<br>'
-                        + 'Vinigar       1/4 cup<br>Soy Sauce     2 Tbspns<br><br>Add asian noodles if you can '
-                        + 'find them'
-      },
-      {
-        name: 'Pytti Pannu',
-        ingredients: '',
-        recipe_text: 'Fry one garlic clove<br>Boil 6 tatties (skin on), peel and cube<br>Fry chicken completely<br>1/2'
-                       + 'tsp curry powder<br>chop nakki<br><br>1/2 tsp paprika powder<br>fry all to get colour<br>'
-                       + 'add 2 raw eggs<br>+ salt<br>'
-
-      },
-      {
-        name: 'Carbonara',
-        ingredients: '',
-        recipe_text: 'Panchetta (Italian fine bacon) - or other salty strips of bacon. Maybe 200g?'
-      }
-  ]
 
   return (
     <div>
---
+++ src/recipes_object.js
@@ -0,0 +1,22 @@
+  export const recipes = [
+      {
+        name: 'Asian Noodle Salad',
+        ingredients: 'Chopped: Spring Onion<br>Chopped: Chinese Cabbage or Lettuce<br>Crushed: Cashew nuts or almonds',
+        recipe_text: 'Mix while heating gently and pour over:<br>Olive Oil     1/2 cup<br>Caster Suger  1/4 cup<br>'
+                        + 'Vinigar       1/4 cup<br>Soy Sauce     2 Tbspns<br><br>Add asian noodles if you can '
+                        + 'find them'
+      },
+      {
+        name: 'Pytti Pannu',
+        ingredients: '',
+        recipe_text: 'Fry one garlic clove<br>Boil 6 tatties (skin on), peel and cube<br>Fry chicken completely<br>1/2'
+                       + 'tsp curry powder<br>chop nakki<br><br>1/2 tsp paprika powder<br>fry all to get colour<br>'
+                       + 'add 2 raw eggs<br>+ salt<br>'
+
+      },
+      {
+        name: 'Carbonara',
+        ingredients: '',
+        recipe_text: 'Panchetta (Italian fine bacon) - or other salty strips of bacon. Maybe 200g?'
+      }
+  ]
