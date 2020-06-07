755317ebd7d02e85b77444010f39ab73ed5ea70d
Allan Wilson <allan@allanwilson.net>
2020-06-07 23:10:12 +0300

    .

--- src/components/walkingRoutes.js
+++ src/components/walkingRoutes.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { Marker, Popup } from "react-leaflet"
 import * as alluMapData from "../data/allu-map-data.json"
-import getAllMapDataFromPostgres from "../DB/connectToPostgresDB"
+//import getAllMapDataFromPostgres from "../DB/connectToPostgresDB"
 
 import { Icon } from "leaflet"
 
@@ -12,7 +12,7 @@ const hikingIcon = new Icon({
 
 const DisplayWalkingRoutes = ( {activeRoute, setActiveRoute} ) => {
     console.log(alluMapData)
-    console.log(getAllMapDataFromPostgres)
+    //console.log(getAllMapDataFromPostgres)
     return (
             <div>
             {alluMapData.walkingRoutes.map(route => (
