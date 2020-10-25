1bde6cac382a1ab45ff92f995675708cf6c4e4ba
Allan Wilson <allan@allanwilson.net>
2020-10-25 23:14:47 +0200

    strava added

--- src/index.js
+++ src/index.js
@@ -69,6 +69,10 @@ const RenderRecipe = ( {recipes} ) => {
         <div>
         <DisplayMap activeRoute={activeRoute} setActiveRoute={setActiveRoute} activeMunro={activeMunro} setActiveMunro={setActiveMunro} />
         </div>
+
+        <div>
+        <iframe height='454' width='300' frameborder='0' allowtransparency='true' scrolling='no' src='https://www.strava.com/athletes/6764130/latest-rides/a4818acb778d736aee592cc0435fda4e8038c80e'></iframe>
+        </div>
      </div>
   )
 
