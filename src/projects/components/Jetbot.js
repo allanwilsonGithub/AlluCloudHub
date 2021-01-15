b42e2b1f1c83b3929bbdcb9a2c5ef39f73518ba0
Allan Wilson <allan@allanwilson.net>
2021-01-15 09:04:51 +0200

    project page

---
+++ src/projects/components/Jetbot.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Jetbot = () => {
+    return (
+        <div align='center'>
+          <h1>Nvidia Jetbot AI Robot platform</h1>
+        </div>
+    )
+}
+
+export default Jetbot
--- src/projects/components/Projects.js
+++ src/projects/components/Projects.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import arch_diag from '../media/AlluCloudHubDocker.png'
+import Jetbot from './Jetbot'
 
 const Projects = () => {
+    const [ ProjectPage, setProjectPage ] = useState('')
     return (
         <div align='center'>
           <h1>Projects</h1>
-          <p className="project_text" align='left'>Jetbot</p>
+          <button key="Jetbot" className="button" onClick={() => setProjectPage('Jetbot')}>Nvidia Jetbot AI</button>
           <p className="project_text" align='left'>Minecraft Mods</p>
           <p className="project_text" align='left'>Roblox Mods</p>
           <p className="project_text" align='left'>FullstackOpen2021</p>
@@ -13,6 +15,7 @@ const Projects = () => {
             <p className="project_text" align='left'>Personal Website (www.allanwilson.net)</p>
             <img src={arch_diag} alt="own website" width="1000" height="500"></img>
           </div>
+          <Jetbot />
         </div>
     )
 }
